edb3e9667359df5eb338a8e772463b512dc195e5
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2019-08-20 15:42:19 -0300

    Implemented basic scope. Implemented commands dimg, if, else, endif, set, println .

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ inline bool is_allowed_in_string( char c ){
 /*
  *  @brief Lista de comandos aceitos pelo interpretador
  */
-const set<string> commands = {"dimg" , "program", "dodin", "dim", "define", "for", "set", "end", "global"};
+const set<string> commands = { "dimg" , "if", "else", "endif", "set", "println" };
 
 class Analizer_ACL{
 public:
@@ -94,7 +94,7 @@ bool Analizer_ACL::check_token_sequence( vector<string> type_sequence ){
             return second->type == first;
         }
     })){
-        token_iterator = after_state;
+        token_iterator = after_state;//Eat the tokens.
         return true;
     }else{
         return false;
@@ -241,13 +241,14 @@ void Analizer_ACL::lexer(){
     int iterador = 0;
 
     do{
-        cout << "Posicaoao do iterador no comeco da analise  "<<std::distance(token_vector.begin(), token_iterator) << endl;
-        cout<< "Start decoding token:    "<< (**token_iterator).type <<endl;
+        cout << "\n\nPosicaoao do iterador no comeco da analise  "<<std::distance(token_vector.begin(), token_iterator) << endl;
+        cout<< "Start decoding token:    "<< (**token_iterator).content << " ------- " << (**token_iterator).type <<endl;
         if ( check_token_sequence({"WHITE SPACE"})){
 
         }else if( check_token_sequence({"NEW LINE"})){
 
             ++line_count;
+
         }else if( check_token_sequence({"_dimg","WHITE SPACE","WORD","INDEX", "NEW LINE"})){
             string map_key, index;
             int number_of_indexes;
@@ -264,9 +265,10 @@ void Analizer_ACL::lexer(){
                 cout << "\nDeclaring global variable named   " << map_key + to_string(i) + "]" << endl;
                 (**scopes.begin())[ map_key + to_string(i) + "]" ] = "";
             }
+
         // }else if( check_token_sequence({"_if", "WHITE SPACE", "WORD", "INDEX", "WHITE SPACE", "_=", "WHITE SPACE", "WORD", "INDEX", "NEW LINE" })){
         }else if( check_token_sequence({"_if", "WHITE SPACE"})){
-            string first_key, second_key;
+            string first_key, first_value, second_key, second_value, used_operator;
             bool condition;
 
             if( check_token_sequence({"WORD","INDEX","WHITE SPACE"}) ){
@@ -274,32 +276,49 @@ void Analizer_ACL::lexer(){
             }else if( check_token_sequence({"WORD", "WHITE SPACE"}) ){
                 first_key = "var_" + (*(token_iterator-2))->content;
             }else{
-                cout << "\n!Expecting variable. Sintax error." << endl;
+                cout << "\n!Expecting variable. Sintax error." << line_count << endl;
             }
+            cout << "First key is  " << first_key << endl;
+            first_value = (**scope_iterator)[first_key];
             
-            if( !check_token_sequence({"OPERATOR"}) ){
-                cout << "\n!!Expecting operator. SIntax error." << endl;
+            if( check_token_sequence({"OPERATOR"}) ){
+                used_operator = (*(token_iterator-1))->content;
+            }else{
+                cout << "\n!!Expecting operator. Sintax error." << line_count << endl;
             }
 
             if( check_token_sequence({ "WHITE SPACE", "WORD", "INDEX", "NEW LINE" }) ){
                 second_key = "var_" + (*(token_iterator-3))->content + (*(token_iterator-2))->content;
+
+            }else if( check_token_sequence({ "WHITE SPACE", "WORD", "NEW LINE" }) ){
+                second_key = "var_" + (*(token_iterator-2))->content;
+
+            }else{
+                cout << "\n!Expecting variable. Sintax error." << line_count << endl;
             }
+            second_value = (**scope_iterator)[second_key];
 
             ++line_count;
 
             //first_key = "var_" + (*(token_iterator-8))->content + (*(token_iterator-7))->content;
             //second_key = "var_" + (*(token_iterator-3))->content + (*(token_iterator-2))->content;
 
-            if( (*(token_iterator-5))->content == "=" ){
-                condition = (**scope_iterator)[first_key] == (**scope_iterator)[ second_key ];
+            cout << "The first value is " << first_value << "  the second value is " << second_value << endl;
+            if( used_operator == "=" ){
+                condition = first_value == second_value;
+            }else if( used_operator == "<"){
+                condition = atoi(first_value.c_str()) < atoi(second_value.c_str()); 
+            }else if( used_operator == ">"){
+                condition = atoi(first_value.c_str()) > atoi(second_value.c_str());
             }
 
+            cout << "\nif condition is " << condition << endl;
             if( condition ){
-                //enter if scope.
-                scopes.push_back(new map<string,string>);
+                //Enter if scope.
+                scopes.push_back( new map<string,string> );
                 ++scope_iterator;
             }else{
-                //in case the condition wasent met, skin the if block.
+                //If case the condition wasent met, skip the if block.
                 do{
                     ++token_iterator;
                     if((*token_iterator)->type == "END"){
@@ -310,7 +329,7 @@ void Analizer_ACL::lexer(){
                 ++token_iterator;//The token skiped was a else to run instead of an if, or it was the endif in case the if didn`t had an else.
             }
                 
-        }else if( check_token_sequence({"_else"}) ){//if is reading else at this point is because it was inside an if, so the else block must be skipped.
+        }else if( check_token_sequence({"_else"}) ){//If is reading else at this point is because it was inside an if, so the else block must be skipped.
             do{
                 ++token_iterator;
                 if((*token_iterator)->type == "END"){
@@ -318,10 +337,37 @@ void Analizer_ACL::lexer(){
                     break;
                 }
             }while( (*token_iterator)->content != "endif");
+            ++token_iterator;//Skip the token that sinalizes the end of the block;
+
+        }else if( check_token_sequence({ "_println", "WHITE SPACE", "STRING", "NEW LINE" })){
+            ++line_count;
+
+            cout << (*(token_iterator-2))->content;
+
+        }else if( check_token_sequence({ "_endif" }) ){    
+            //If it hit an endif here it is geting out of a scope, then pop the scope
+            --scope_iterator;
+            scopes.pop_back();
+
+        }else if( check_token_sequence({ "_set", "WHITE SPACE", "WORD" }) ){
+            string key, value;
+
+            if( check_token_sequence({ "INDEX", "WHITE SPACE", "_=", "WHITE SPACE", "NUMBER" }) ){
+                key = "var_" + (*(token_iterator-6))->content + (*(token_iterator-5))->content;
+                value = (*(token_iterator-1))->content;
+            }else if( check_token_sequence({ "WHITE SPACE", "_=", "WHITE SPACE", "NUMBER" }) ){
+                key = "var_" + (*(token_iterator-5))->content;
+                value = (*(token_iterator-1))->content;
+            }
+
+            (**scope_iterator)[key]= value;
+            cout << "new value is " << key << " = " << (**scope_iterator)[key] << endl;
+
         }else if((*token_iterator)->type == "END"){
             break;
         }
         ++iterador;
+
     }while( iterador < 20);
 
 }
